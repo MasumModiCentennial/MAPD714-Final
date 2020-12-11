bd7fd5d74609af8e3eccb9bd8801a4cf54a960d2
masummodi <masum.modi@yobored.com>
2020-12-11 02:27:56 -0500

    first commit

---
+++ BMI Calculator/BMIRecords/BMIRecordsController.swift
@@ -0,0 +1,9 @@
+//
+//  BMIRecordsController.swift
+//  BMI Calculator
+//
+//  Created by Masum Modi on 2020-12-11.
+//  Copyright © 2020 Centennial College. All rights reserved.
+//
+
+import Foundation
